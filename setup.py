cdbdceff00facd52c99e6c7f2157a9b3e6eb71c1
Henry Lok <ihenrylok@gmail.com>
2025-07-20 22:17:07 +0800

    0.9.1

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='codestate',
-    version='0.9.0',
+    version='0.9.1',
     description='A CLI tool for codebase statistics and ASCII visualization',
     author='Henry Lok',
     packages=find_packages(),
-    install_requires=['pathspec', 'openpyxl', 'rich'],
+    install_requires=['pathspec', 'openpyxl'],
     entry_points={
         'console_scripts': [
             'codestate=codestate.cli:main',  # This entry point allows users to run `codestate` in the terminal after installation.
